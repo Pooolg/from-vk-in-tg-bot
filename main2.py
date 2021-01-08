2793f1deb52d7b9acfe519901643084073eae2d2
Pooolg <59968996+Pooolg@users.noreply.github.com>
2021-01-08 17:40:33 +0500

    Create main2.py

---
+++ main2.py
@@ -0,0 +1,38 @@
+import requests
+import time
+import os
+
+
+vk_group = "writehere"
+vk_token = "writehere"
+bot_token = "writehere"
+channel = "@channel"
+
+
+
+lastUrl = ""
+while True:
+    try:
+        r = requests.get("https://api.vk.com/method/wall.get", params={"access_token":vk_token,"domain":vk_group,"v":"5.92","count":"1"}).json()["response"]["items"][0]["attachments"]
+        picUrl= r[0]['photo']['sizes'][-1]['url']
+        if picUrl == lastUrl:
+            print("Нет новых публикаций")
+            time.sleep(300)
+        else:
+            lastUrl = picUrl
+            print("Появилась новая публикация")
+            r = requests.get(picUrl)
+            inp = open('pic/1.jpg', "wb")
+            inp.write(r.content)
+            inp.close()
+            out = {'photo': open('pic/1.jpg', 'rb')}
+            data = {'chat_id': "@artforyoulife"}
+            r = requests.post("https://api.telegram.org/bot"+bot_token+"/sendPhoto", files=out, data=data)
+            out['photo'].close()
+            os.remove("pic/1.jpg")
+            print("Отправлено")
+            time.sleep(300)
+    except LookupError:
+        print("Пустая публикация/нет картинок/пересылка")
+        print("жду час...")
+        time.sleep(3600)
