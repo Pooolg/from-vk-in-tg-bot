f1b74e07d350359551a60541b0eb3203a91c85db
Pooolg <59968996+Pooolg@users.noreply.github.com>
2021-01-08 18:34:50 +0500

    Update main2.py
    fix crash

--- main2.py
+++ main2.py
@@ -10,23 +10,23 @@ channel = "@channel"
 
 
 
-lastUrl = ""
+lastpost = ""
 while True:
     try:
         r = requests.get("https://api.vk.com/method/wall.get", params={"access_token":vk_token,"domain":vk_group,"v":"5.92","count":"1"}).json()["response"]["items"][0]["attachments"]
         picUrl= r[0]['photo']['sizes'][-1]['url']
-        if picUrl == lastUrl:
+        if r[0]['photo']['post_id'] == lastpost:
             print("Нет новых публикаций")
             time.sleep(300)
         else:
-            lastUrl = picUrl
+            lastpost = r[0]['photo']['post_id']
             print("Появилась новая публикация")
             r = requests.get(picUrl)
             inp = open('pic/1.jpg', "wb")
             inp.write(r.content)
             inp.close()
             out = {'photo': open('pic/1.jpg', 'rb')}
-            data = {'chat_id': "@artforyoulife"}
+            data = {'chat_id': channel}
             r = requests.post("https://api.telegram.org/bot"+bot_token+"/sendPhoto", files=out, data=data)
             out['photo'].close()
             os.remove("pic/1.jpg")
